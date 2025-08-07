00494d992f19edef5828d05f63dbb272499b60f5
S_fy <samuel.fey.dev@gmail.com>
2025-08-07 13:51:45 -0300

    Changed database to postgres

--- projeto/projeto/settings.py
+++ projeto/projeto/settings.py
@@ -75,13 +75,24 @@ WSGI_APPLICATION = 'projeto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#   }
+#}
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('caamsql'),
+        'USER': os.getenv('s-fey'),
+        'PASSWORD': os.getenv('us5csHsxxT4KowndawKCm4RrqNSjXBtb'),
+        'HOST': os.getenv('dpg-d2addn6r433s73acqjrg-a'),
+        'PORT': os.getenv('5432'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
