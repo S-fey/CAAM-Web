c1902a3bc178081d87df01c86c2c04a5a95c0a8f
S_fy <samuel.fey.dev@gmail.com>
2025-08-07 16:30:25 -0300

    Redeploy

--- projeto/projeto/settings.py
+++ projeto/projeto/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'projeto.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
+# old database:
 #DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
